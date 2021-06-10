9915a1939bc562e7c3b395d78d1c30a18e535d6f
Paul Irish <commits@paul.irish>
2021-06-10 16:05:15 -0700

    apply patch

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+    env: {
+        browser: true,
+    },
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2015,
+    },
+    rules: {
+        'indent': ['error', 4],
+        'capitalized-comments': 'off',
+    },
+}
